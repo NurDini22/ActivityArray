420c77b1c6e8f2b79b37f76edfcc4cfb82a1fb8d
Nur Dini Binti Azhar <18DDT19F1138@student.ptss.edu.my>
2021-04-14 13:09:25 +0800

    Unfinished Coding 14/4/2021

---
+++ Activity Array/Form2.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblArrayTwoDimensi = New System.Windows.Forms.Label()
+        Me.btnPaparTwoDimensi = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblArrayTwoDimensi
+        '
+        Me.lblArrayTwoDimensi.AutoSize = True
+        Me.lblArrayTwoDimensi.Location = New System.Drawing.Point(321, 184)
+        Me.lblArrayTwoDimensi.Name = "lblArrayTwoDimensi"
+        Me.lblArrayTwoDimensi.Size = New System.Drawing.Size(51, 17)
+        Me.lblArrayTwoDimensi.TabIndex = 0
+        Me.lblArrayTwoDimensi.Text = "Label1"
+        '
+        'btnPaparTwoDimensi
+        '
+        Me.btnPaparTwoDimensi.Location = New System.Drawing.Point(118, 184)
+        Me.btnPaparTwoDimensi.Name = "btnPaparTwoDimensi"
+        Me.btnPaparTwoDimensi.Size = New System.Drawing.Size(167, 40)
+        Me.btnPaparTwoDimensi.TabIndex = 1
+        Me.btnPaparTwoDimensi.Text = "Papar Array 2 Dimensi"
+        Me.btnPaparTwoDimensi.UseVisualStyleBackColor = True
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnPaparTwoDimensi)
+        Me.Controls.Add(Me.lblArrayTwoDimensi)
+        Me.Name = "Form2"
+        Me.Text = "Form2"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblArrayTwoDimensi As Label
+    Friend WithEvents btnPaparTwoDimensi As Button
+End Class
---
+++ Activity Array/Form2.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form2
+    Private Sub btnPaparTwoDimensi_Click(sender As Object, e As EventArgs) Handles btnPaparTwoDimensi.Click
+        Dim strKelas(5, 3) As String
+
+        strKelas(0, 0) = "DDT1A"
+        strKelas(0, 1) = "DDT1B"
+        strKelas(1, 0) = "DDT2A"
+        strKelas(1, 1) = "DDT2B"
+        strKelas(1, 2) = "DDT2C"
+
+        lblArrayTwoDimensi.Text = strKelas(0, 1)
+
+    End Sub
+End Class
